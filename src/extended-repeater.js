3d323c40c9a7eb1ebb1efef4ecac6295ef9aa73d
Alla Borisova <alla.borisova27@gmail.com>
2022-05-02 11:55:58 +0500

    extended repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let result = ''
+  // console.log(options.repeatTimes);
+  let sep = (options.separator) ? options.separator : '+';
+  let addsep = (options.additionSeparator) ? options.additionSeparator : '|';
+  let rep = (options.repeatTimes) ? options.repeatTimes : 1;
+  let addrep = 0;
+  if (options.addition !== undefined) {
+    addrep = (options.additionRepeatTimes) ? options.additionRepeatTimes : 1;
+  }
+  // console.log(sep);
+  for (let i = 0; i < rep; i++) {
+    let addstr = '';
+    for (let j = 0; j < addrep; j++) {
+
+      if (j === addrep - 1) {
+        addstr += options.addition;
+      } else {
+        addstr += options.addition + addsep;
+      }
+    }
+    if (i === rep - 1) {
+      result += str + addstr;
+    } else {
+      result += str + addstr + sep;
+    }
+
+  }
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  return result;
 }
 
 module.exports = {
